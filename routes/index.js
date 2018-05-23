35c0607b40ac8a946346ddbbb6d360e7ca49add1
Tomo Masakura <tomo.masakura@gmail.com>
2018-05-23 14:25:57 +0900

    購入した場合の通知を実装

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ webPush.setVapidDetails('mailto:tomo.masakura+webpush@gmail.com', vapidKeys.publ
 
 const pushManager = new PushManager();
 
+const wait = miliseconds => new Promise(resolve => setTimeout(resolve, miliseconds));
+
 router.get('/keys/public', (req, res) => {
   res.json({
     publicKey: vapidKeys.publicKey,
@@ -25,6 +27,28 @@ router.post('/echo', (req, res) => {
   });
 
   pushManager.sendRandomDelay(req.body, message);
+});
+
+router.post('/buy', (req, res) => {
+  res.send(JSON.stringify({}));
+
+  (async () => {
+    await wait(5000);
+
+    pushManager.send({
+      title: '配達中',
+      body: 'ただいま配達中です。',
+      data: Object.assign({}, req.body.data, { state: 'delivering' }),
+    });
+
+    await wait(4000);
+
+    pushManager.send({
+      title: '配達完了',
+      body: '配達が完了しました。',
+      data: Object.assign({}, req.body.data, { state: 'complete' }),
+    });
+  })();
 });
 
 module.exports = router;
